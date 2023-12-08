7e9c5c94fcc0c7fd10a9814f4faf63c4938e8655
Coolhacker009 <jackberthe009@gmail.com>
2023-12-08 11:48:18 +0000

    last last 2

--- forms/contact.php
+++ forms/contact.php
@@ -8,11 +8,11 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'alhassanberthe009@gmail.com';
-    
+
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Coming Soon!');
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
